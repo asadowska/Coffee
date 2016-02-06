a2672503e99d8f288a8ddfe62571f13776315545
Nemeczek <Nemeczekes@hotmail.com>
2016-02-06 12:30:03 +0100

    initial file reading to dictionary

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace ConsoleApplication
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApplication
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var coffeesDescriptions = new Dictionary<string, string>();
+
+			var lines = File.ReadLines("cofee_names.csv");
+
+			foreach (var line in lines)
+			{
+				var partsOfLine = line.Split(';');
+				coffeesDescriptions.Add(partsOfLine[0],partsOfLine[1]);
+			}
 		}
 	}
 }
